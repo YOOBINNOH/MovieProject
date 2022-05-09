26217f53f0374beda9da946f0fa8d23205feae57
YOOBINNOH <kuninyb@naver.com>
2022-05-09 11:40:15 +0900

    제목 크롤링 및 각 리스트 생성 완성

--- crawling.py
+++ crawling.py
@@ -1,17 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
+title = [ ] # 영화 제목 리스트
+movie_rate = [ ] # 영화 등급 
+netizen_rate = [ ] # 네티즌 평점
+netizen_count = [ ] # 네티즌 평점 참여자 수
+journalist_score = [ ] # 기자평론가 평점
+journalist_count = [ ] # 기자평론가 참여자수
+scope = [ ] # 개요
+playing_time = [ ] # 상영시간
+opening_date = [ ] # 개봉날짜
+director = [ ] # 감독
+image = [ ] # 영화 대표 이미지 주소
+
+
 url = 'https://movie.naver.com/movie/running/current.naver'
 
 response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
 
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    title=soup.select_one('#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dt > a')
-    print(title.getText())
-    
-else : 
-    print(response.status_code)
+# 제목 추출
+k=soup.select_one('#content > div.article > div:nth-child(1) ')
+titles = k.select('li > dl > dt > a')
+for i in titles:
+    title.append(i.get_text())
 
+
+#content > div.article > div:nth-child(1)
+#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dt
 
