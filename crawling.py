3d4bb216b0cd37a6c78d787cc973e27019b9563d
YOOBINNOH <kuninyb@naver.com>
2022-05-10 15:12:16 +0900

    등급 추출 후 리스트에 넣기 완료. len(scope)랑 len(title) 같음

--- crawling.py
+++ crawling.py
@@ -20,6 +20,8 @@ response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 
+
+
 # 제목 추출
 k=soup.select_one('#content > div.article > div:nth-child(1) ')
 titles = k.select('li > dl > dt > a')
@@ -27,17 +29,36 @@ for i in titles:
     title.append(i.get_text())
 
 
-# 등급 추출
+
+
+# 등급 추출 후 출력 값 변경해서 리스트에 넣기
 a=soup.select_one('#content > div.article > div:nth-child(1) ')
-scopes = a.select('li > dl > dt > span')
+scopes = a.select('li > dl > dt')
 for i in scopes:
-    if i =='null':
-        scope.append("")
+    s = i.select_one('span')
     scope.append(i.get_text())
 
-print(len(title))
-print(len(scope))
+for i in range(0,len(scope)):
+    if scope[i][2]=='5':
+        scope[i]="15세 관람가"
+    elif scope[i][2]=='2':
+        scope[i]='12세 관람가'
+    elif scope[i][2]=='소':
+        scope[i]='청소년 관람 불가'
+    elif scope[i][2]=='체':
+        scope[i]='전체 관람가'
+    else:
+        scope[i]='정보 없음'                
 
 
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dt > a
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dt > span
+
+# 네티즌 평점 추출
+b=soup.select_one('#content > div.article > div:nth-child(1) > div.lst_wrap > ul ')
+b=b.find_all(class_="num")
+
+print(b[0])
+print(b[1])
+print(b[2])
+
+#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > dl > dd.star > dl > dd:nth-child(2) > div > a > span.num
+#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd.star > dl > dd:nth-child(2) > div > a > span.num
