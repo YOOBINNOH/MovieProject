c09a09ddcc8a547a20a64203303f0120bae5682b
YOOBINNOH <kuninyb@naver.com>
2022-05-12 08:59:54 +0900

    개요 , 감독 완성. 중복 값을 반복문을 통해 처리

--- crawling.py
+++ crawling.py
@@ -22,13 +22,18 @@ soup = BeautifulSoup(html, 'html.parser')
 
 
 
+
+
+
 # 제목 추출
 k=soup.select_one('#content > div.article > div:nth-child(1) ')
 titles = k.select('li > dl > dt > a')
 for i in titles:
     title.append(i.get_text())
+print("제목 수 : ", len(title))
+    
 
-    #content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(28) > dl > dt > a
+
 
 
 
@@ -39,29 +44,40 @@ scopes = a.select('li > dl > dt')
 
 for i in scopes:
     s = i.select_one('span')
-    scope.append(i.get_text())
+    movie_rate.append(i.get_text())
 
 for i in range(0,len(scope)):
-    if scope[i][2]=='5':
-        scope[i]="15세 관람가"
-    elif scope[i][2]=='2':
-        scope[i]='12세 관람가'
-    elif scope[i][2]=='소':
-        scope[i]='청소년 관람 불가'
-    elif scope[i][2]=='체':
-        scope[i]='전체 관람가'
+    if movie_rate[i][2]=='5':
+        movie_rate[i]="15세 관람가"
+    elif movie_rate[i][2]=='2':
+        movie_rate[i]='12세 관람가'
+    elif movie_rate[i][2]=='소':
+        movie_rate[i]='청소년 관람 불가'
+    elif movie_rate[i][2]=='체':
+        movie_rate[i]='전체 관람가'
     else:
-        scope[i]='정보 없음'                
+        movie_rate[i]='정보 없음'                
+print("등급 수 : ",len(movie_rate))
+
+
+
+
+
+
 
 # 네티즌 평점 
 
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(29) > dl > dd.star > dl > dd:nth-child(2) > div > a > span.num
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(30) > dl > dd.star > dl > dd:nth-child(2) > div > a > span.num
+
 net=soup.select_one('#content > div.article > div:nth-child(1) ')
 net=net.select('li > dl > dd.star > dl > dd:nth-child(2) > div > a >span.num')
 
 for i in net:
     netizen_rate.append(float(i.get_text()))
+
+print("네트즌 평점 수 : ", len(netizen_rate))
+
+
+
 
 
 
@@ -73,6 +89,11 @@ netnum=netnum.select('li > dl > dd.star > dl > dd:nth-child(2) > div > a > span.
 
 for i in netnum:
     netizen_count.append(i.get_text())
+
+print("네트즌 평점 참여자 수 : ", len(netizen_count))
+
+
+
 
 
 
@@ -88,10 +109,12 @@ for i in jr:
         journalist_score.append("정보 없음")
     else:
         journalist_score.append(float(s.get_text()))
-    
+print("기자 평론가 평점 수 : ",len(journalist_score))    
 
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd.star > dl > dd:nth-child(4) > div > a > span.num
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > dl > dd.star > dl > dd:nth-child(4) > div > a > span.num
+
+
+
+
 
 
 
@@ -105,42 +128,49 @@ for i in jrnum:
     else:
         journalist_count.append(s.get_text())   
  
+print("기자 평론가 참여자 수 : ",len(journalist_count))
 
 
 
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd.star > dl > dd:nth-child(4) > div > a > span.num2 > em
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > dl > dd.star > dl > dd:nth-child(4) > div > a > span.num2 > em
+
 
 
 # 개요 - 작업 중 : 중복 값 문제
 geyo = soup.select_one("#content > div.article > div:nth-child(1) > div.lst_wrap > ul")
-geyo = geyo.select('li > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a')
-for i in geyo:
-    scope.append(i)
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(3) > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a
+geyo = geyo.select('li > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt ')
 
+strs = ''
+for i in geyo:
+    k = i.find_all('a')
+    for j in k:
+        strs += str( j.get_text()+" " )
+    scope.append(strs)    
+print('개요 수 : ', len(scope))  
 
 
 
 # 감독 - 작업 중 : 중복 값 문제
 dir = soup.select_one('#content > div.article > div:nth-child(1) > div.lst_wrap > ul')
-dir = dir.select('li > dl > dd:nth-child(3) > dl > dd:nth-child(4) > span > a')
+dir = dir.select('li > dl > dd:nth-child(3) > dl > dd:nth-child(4) > span ')
+st = ""
 for i in dir:
-    director.append(i)
+    k = i.find_all('a')
+    for j in k:
+        st += str( j.get_text()+" " )
+    director.append(st)    
 
+print("감독 수 : ",(len(director)))
 
 #content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd:nth-child(3) > dl > dd:nth-child(4) > span > a
 #content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > dl > dd:nth-child(3) > dl > dd:nth-child(4) > span > a
 
 
-# 영화 대표 이미지 주소 
+# 영화 대표 이미지 주소 : 119개 값 나옴
 img = soup.select_one('#content > div.article > div:nth-child(1) > div.lst_wrap > ul ')
-img = img.select('li > div > a ')
+img = img.select('li > div  ')
 for i in img:
     image.append(i.get_text())
     
 
-print(len(image))
 #content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(3) > div > a > 
 #content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(4) > div > a > img
